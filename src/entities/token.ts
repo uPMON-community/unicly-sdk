78537a19562245693e313bc9dac49a696efbf4af
uPMON-community <upmon.community@protonmail.com>
2022-02-17 08:59:09 +0100

    Add wrapped matic

--- src/entities/token.ts
+++ src/entities/token.ts
@@ -79,5 +79,7 @@ export const WETH = {
     'Wrapped Ether'
   ),
   [ChainId.GÖRLI]: new Token(ChainId.GÖRLI, '0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6', 18, 'WETH', 'Wrapped Ether'),
-  [ChainId.KOVAN]: new Token(ChainId.KOVAN, '0xd0A1E359811322d97991E03f863a0C30C2cF029C', 18, 'WETH', 'Wrapped Ether')
+  [ChainId.KOVAN]: new Token(ChainId.KOVAN, '0xd0A1E359811322d97991E03f863a0C30C2cF029C', 18, 'WETH', 'Wrapped Ether'),
+  [ChainId.MATIC]: new Token(ChainId.MATIC, '0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270', 18, 'WMATIC', 'Wrapped Matic'),
+  [ChainId.MUMBAI]: new Token(ChainId.MUMBAI, '0x9c3c9283d3e44854697cd22d3faa240cfb032889', 18, 'WMATIC', 'Wrapped Matic'),
 }
